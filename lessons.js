37a594aa3c12e5ad98a4fc7a5b8ea1243b80cd0b
umar109 <118481522+umar109@users.noreply.github.com>
2022-11-17 20:19:17 +0000

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,86 @@
+var lessons = [
+{
+    "id": 1,
+    "Subject": "English",
+    "Location": "Glasgow",
+    "Price": 10.99,
+    "Space": 5,
+    "img": "english.jpg"
+},
+{
+    "id": 2,
+    "Subject": "Maths",
+    "Location": "Madrid",
+    "Price": 9.99,
+    "Space": 5,
+    "img":"maths.jpg"
+},
+
+{
+    "id": 3,
+    "Subject": "Spanish",
+    "Location": "London",
+    "Price": 20.99,
+    "Space": 5,
+    "img": "spanish.jpg"
+},
+{
+    "id": 4,
+    "Subject": "PE",
+    "Location": "Barcelona",
+    "Price": 11.99,
+    "Space": 5,
+    "img": "pe.png"
+},
+{
+    "id": 5,
+    "Subject": "IT",
+    "Location": "Rome",
+    "Price": 30.99,
+    "Space": 5,
+    "img": "it.jpg"
+},
+
+{
+    "id": 6,
+    "Subject": "Geography",
+    "Location": "California",
+    "Price": 2.99,
+    "Space": 5,
+    "img": "geography.jpg"
+},
+{
+    "id": 7,
+    "Subject": "History",
+    "Location": "New York",
+    "Price": 10.99,
+    "Space": 5,
+    "img": "history.png"
+},
+{
+    "id": 8,
+    "Subject": "RE",
+    "Location": "Boston",
+    "Price": 23.99,
+    "Space": 5,
+    "img": "re.jpg"
+},
+{
+    "id": 9,
+    "Subject": "Business",
+    "Location": "Manchester",
+    "Price": 24.99,
+    "Space": 5,
+    "img": "business.jpg"
+},
+
+{
+    "id": 10,
+    "Subject": "Science",
+    "Location": "New Castle",
+    "Price": 19.99,
+    "Space": 5,
+    "img": "science.jpg"
+}
+
+]
